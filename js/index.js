a2c4ed2aea0a04c19c3cf718259e76c5664f4f7f
Henry Kaufman <hkaufman19@gmail.com>
2015-07-12 17:54:21 -0600

    updated for firebase

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-var app = angular.module('app', ['app']);
+var app = angular.module('app', ['firebase']);
 
 app.controller('HomeCtrl', ['$scope', '$firebaseArray', function ($scope, $firebaseArray) {
     var ref = new Firebase('https://eakjb-shout-ninja2.firebaseio.com/projects');
-    $scope.data = $firebaseArray(ref);
+    $scope.projects = $firebaseArray(ref);
 }]);
